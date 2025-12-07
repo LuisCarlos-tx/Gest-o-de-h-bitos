a196157c701db718c707cf9cad24c21488bdf870
Luis Carlos <luiscarlostvx34@gmail.com>
2025-12-06 22:45:47 -0300

    item.java

---
+++ item.java
@@ -0,0 +1,36 @@
+public class Item {
+    private String tipo;      
+    private String descricao;
+    private boolean concluido;
+
+  
+    public Item(String tipo, String descricao) {
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.concluido = false;
+    }
+
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluido() {
+        return concluido;
+    }
+
+   
+    public void marcarComoConcluido() {
+        this.concluido = true;
+    }
+
+    @Override
+    public String toString() {
+        String status = concluido ? "✅ CONCLUÍDO" : "⏳ PENDENTE";
+        return "Tipo: " + tipo + " | Descrição: " + descricao + " | Status: " + status;
+    }
+}
